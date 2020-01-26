2560e5c30b122e90749668b7d765c1e12713365c
Adam Boothroyd <adamboothroyd1@gmail.com>
2020-01-26 11:18:47 +0000

    Adding ICM engine

---
+++ sngegt/IcmIntegrator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SNGEGT
+{
+    public class IcmIntegrator
+    {
+        Game game;
+        public IcmIntegrator()
+        {
+            game = new Game();
+        }
+
+        public double GetExpectedValue()
+        {
+            return 0;
+        }
+
+
+        private void SetRanges()
+        {
+            BlindInfo blinds = new BlindInfo(100,200,0);
+
+            game.ICMRanges(blinds, false, (Award)comboBoxGame.SelectedItem, radioButtonPush.Checked);
+        }
+
+    }
+}
